0362769b41ac9cd7f2cc9debec94f0983614ed7e
hongpeng@sina.com <hongpeng@sina.com>
2016-05-21 19:11:49 +0800

    搭建界面

---
+++ xmpp_Client_test/HPContactViewController.h
@@ -0,0 +1,13 @@
+//
+//  HPContactViewController.h
+//  xmpp_Client_test
+//
+//  Created by Apeng on 16/5/21.
+//  Copyright © 2016年 Apeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPContactViewController : UITableViewController
+
+@end
